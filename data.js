3ad0ce99f8c1e9f027174f8ecca3c81c9f5279f5
Matt Wright <nightwing.0189@gmail.com>
2025-12-14 12:33:02 +1000

    Fresh commit with current files

---
+++ data.js
@@ -0,0 +1,58 @@
+// data.js
+const locations = [
+  {
+    id: 1,
+    name: "Effie Yeaw Nature Center",
+    lat: 38.61762484412594,
+    lng: -121.31156087325488,
+    description: "Nature Trail",
+     images: [
+      "images/EffieYeawNatureCenter.jpg"
+    ],
+    videos: [
+      "videos/Test1.mp4",
+      "videos/Test2.mp4"
+    ]
+  },
+  {
+    id: 2,
+    name: "Test Number 2",
+    lat: 38.588809,
+    lng: -121.463154,
+    description: "More Walking Trails",
+     images: [],
+    videos: [
+      "videos/Video1PartA.mp4",
+      "videos/Video1PartB.mp4",
+      "videos/Video1PartC.mp4"
+    ]
+  },
+  {
+    id: 3,
+    name: "Test Number 3",
+    lat: 38.545914,
+    lng: -121.546721,
+    description: "Even More Walking Trails",
+     images: [],
+    videos: [
+      "videos/Video2PartA.mp4",
+      "videos/Video2PartB.mp4",
+      "videos/Video2PartC.mp4"
+    ]
+  },
+  {
+    id: 4,
+    name: "Test Number 4",
+    lat: 38.573119,  
+    lng: -121.328059,
+    description: "More More More Walking Trails",
+     images: [],
+    videos: [
+      "videos/Video2PartA.mp4",
+      "videos/Video2PartB.mp4",
+      "videos/Video2PartC.mp4",
+      "videos/Video2PartD.mp4",
+      "videos/Video2PartE.mp4"
+    ]
+  }
+];
